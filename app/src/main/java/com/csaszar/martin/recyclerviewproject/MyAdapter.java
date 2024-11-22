55e17797210df90cc28af32f73b752784d62d8bb
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2024-11-22 12:49:06 +0100

    commented project

--- app/src/main/java/com/csaszar/martin/recyclerviewproject/MyAdapter.java
+++ app/src/main/java/com/csaszar/martin/recyclerviewproject/MyAdapter.java
@@ -12,6 +12,29 @@ import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
 
+    // ViewHolder osztály: Az egyes elemek nézeteinek létrehozása és újrahasznosítása
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView textView;
+
+        // Nézetek hozzákötése a ViewHolderhez
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+
+    // Az adatok, amiket megjelenítünk (nevek listája)
+    private List<String> nameList;
+
+    // Adatok átadása
+    public MyAdapter(List<String> nameList) {
+        this.nameList = nameList;
+    }
+
+    //RecyclerView.Adapter... osztály metódusainek implementálása
+
+    //Új nézet létrehozása a sablonunk (recycler_item.xml) alapján
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -20,29 +43,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
         return new MyViewHolder(itemView);
     }
 
+    //Adatok hozzárendelése a nézethez
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         String name = nameList.get(position);
     }
 
+    //ez határozza meg, hogy hányszor kell végrehajtani a nézetlétrehozást
     @Override
     public int getItemCount() {
         return nameList.size();
-    }
-
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
-
-        public TextView textView;
-
-        public MyViewHolder(@NonNull View itemView) {
-            super(itemView);
-            textView = itemView.findViewById(R.id.textView);
-        }
-    }
-
-    private List<String> nameList;
-
-    public MyAdapter(List<String> nameList) {
-        this.nameList = nameList;
     }
 }
